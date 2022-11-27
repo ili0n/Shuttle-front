75815bdd4d4b422ba41d8f2f89a54ad3cd128f13
magley <60552482+magley@users.noreply.github.com>
2022-11-27 16:04:46 +0100

    chore: Add material components to project

--- Shuttle-front/src/app/app.module.ts
+++ Shuttle-front/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button'
 
 @NgModule({
     declarations: [
@@ -13,6 +17,10 @@ import { AppComponent } from './app.component';
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+
+        MatToolbarModule,
+        MatButtonModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
