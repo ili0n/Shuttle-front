8cb12bce137e1e07beebd8e515fa5e7edbb7a24c
magley <magazinlazar2@gmail.com>
2022-11-28 19:31:20 +0100

    feat: Add designed login component

--- Shuttle-front/src/app/app.module.ts
+++ Shuttle-front/src/app/app.module.ts
@@ -6,26 +6,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card'
+
 import { LoginComponent } from './login/component/login/login.component'
 
-
 @NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
 
-        MatToolbarModule,
-        MatButtonModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+    // `Material Components` modules are imported here.
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Shuttle-front/src/app/login/component/login/login.component.ts
+++ Shuttle-front/src/app/login/component/login/login.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = "";
+  password: string = "";
 
+  onLoginSubmit(): void {
+    alert(this.email + ' ' + this.password);
+  }
+
+  constructor() {
+    // HACK: Is there a better way to dynamically change body style from a component?
+    document.body.className = "body-gradient1"; // Defined in src/styles.css
+  }
+
+  ngOnDestroy() {
+    document.body.className = "";
+  }
 }
