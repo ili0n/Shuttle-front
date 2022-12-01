49284d682331e17e3a45b8e9e35602b4a2454d43
magley <magazinlazar2@gmail.com>
2022-12-01 13:34:12 +0100

    feat: Create environment.ts
    No more pinging the server socket directly.

--- Shuttle-front/src/app/app.component.ts
+++ Shuttle-front/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
     constructor(private httpClient: HttpClient) { }
 
     helloBackend() {
-        const obs: Observable<Object> = this.httpClient.get("http://localhost:8080/hello", {
+        const obs: Observable<Object> = this.httpClient.get(environment.serverOrigin + "hello", {
             observe: "body",
             responseType: "text",
         });
---
+++ Shuttle-front/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: false,
+	serverOrigin: 'http://localhost:8080/',
+};
