1d876a08fd65b52d675503294d1f82d7f1acc40e
ili0n <urosstanic25@gmail.com>
2022-12-07 21:23:17 +0100

    +onclick routing

--- Shuttle-front/src/app/navbar/navbar.component.ts
+++ Shuttle-front/src/app/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Attribute, Component, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  constructor(private router: Router){
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  loginRoute() {
+    this.router.navigate(["/login"]);
+  
+  }
+  registerRoute() {
+    this.router.navigate(["/register"]);
+  }
+  homeRoute(){
+    this.router.navigate([""]);
+  }
 }
