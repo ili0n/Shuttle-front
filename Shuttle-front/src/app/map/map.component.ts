af3d449a728f1aaa7b9e647e3d0715d8ddce92dd
unknown <nemanja.majstorovic3214@gmail.com>
2022-12-17 10:57:21 +0100

    Added map

---
+++ Shuttle-front/src/app/map/map.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+  private map?: L.Map;
+
+  private initMap(): void{
+    this.map = L.map("unregistered-map", {
+      center: [ 45.267136, 19.833549 ],
+      zoom: 3
+    })
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor(){}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+}
