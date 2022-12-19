4683af8e60da0e354305afedea4f1c583e361531
magley <magazinlazar2@gmail.com>
2022-12-19 19:44:10 +0100

    feat: Add basic page layout

--- Shuttle-front/src/app/driver/driver-home/driver-home.component.ts
+++ Shuttle-front/src/app/driver/driver-home/driver-home.component.ts
@@ -1,32 +1,67 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Passenger, PassengerService } from 'src/app/passenger/passenger.service';
 import { environment } from 'src/environments/environment';
+
+interface RideRequestPassenger {
+    id: number,
+    email: string,
+}
+
+interface RideRequestSingleLocation {
+    address: string,
+    latitude: number,
+    longitude: number,
+}
+
+interface RideRequestLocation {
+    departure: RideRequestSingleLocation,
+    destination: RideRequestSingleLocation,
+}
+
+interface RideRequest {
+    passengers: Array<RideRequestPassenger>,
+    locations: Array<RideRequestLocation>,
+    babyTransport: boolean,
+    petTransport: boolean,
+}
 
 @Component({
     selector: 'app-driver-home',
     templateUrl: './driver-home.component.html',
     styleUrls: ['./driver-home.component.css']
 })
-export class DriverHomeComponent implements OnInit {
+export class DriverHomeComponent implements OnInit, OnDestroy {
+    requests: Array<RideRequest> = [];
 
+    constructor(private httpClient : HttpClient, private passengerService: PassengerService) {
+    }
 
-    constructor(private httpClient : HttpClient) {
+    ngOnDestroy() {
+        document.body.className = "";
     }
 
     ngOnInit(): void {
+        document.body.className = "body-gradient1";
         this.subscribeToRides();
+    }
+
+    getRoutePoints(request: RideRequest): Array<RideRequestSingleLocation> {
+        let res = request.locations.map(l => l.departure);
+        res.push(request.locations[request.locations.length - 1].destination);
+        return res;
     }
 
     subscribeToRides() {
         // TODO: Get driver ID from session.
         let path: string = 'api/ride/driver/' + 1 + '/ride-requests';
 
-        const obs: Observable<Object> = this.httpClient.get(environment.serverOrigin + path, {
+        const obs: Observable<Array<RideRequest>> = this.httpClient.get<Array<RideRequest>>(environment.serverOrigin + path, {
             observe: "body",
             responseType: "json",
         });
 
-        obs.subscribe(result => console.log(result));
+        obs.subscribe((receivedData: Array<RideRequest>) => this.requests = receivedData);
     }
 }
