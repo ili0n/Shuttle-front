bb007098a99315ab401b787452f982931f2b816b
uros <urosstanic25@gmail.com>
2022-12-28 21:13:52 +0100

    faulty map implementation

--- Shuttle-front/src/app/driver/driver-ride-history/driver-ride-history/driver-ride-history.component.ts
+++ Shuttle-front/src/app/driver/driver-ride-history/driver-ride-history/driver-ride-history.component.ts
@@ -1,74 +1,45 @@
-import {Component} from '@angular/core';
-import {AuthService} from "../../../auth/auth.service";
-import {Router} from "@angular/router";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {environment} from "../../../../environments/environment";
+import {AfterViewInit, Component} from '@angular/core';
 import {DriverRideHistoryService, Ride} from "./driver-ride-history.service";
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
     selector: 'app-driver-ride-history',
     templateUrl: './driver-ride-history.component.html',
     styleUrls: ['./driver-ride-history.component.css']
 })
-export class DriverRideHistoryComponent {
+export class DriverRideHistoryComponent implements AfterViewInit {
+
+    private map: any;
 
     constructor(private rideHistoryService: DriverRideHistoryService) {
     }
 
-    // rides = [{
-    //     id: "ride1",
-    //     price: 1250,
-    //     startDate: new Date("07/21/2024"),
-    //     departure: {
-    //         address: "Bulevar oslobodjenja 46",
-    //         "latitude": 45.267136,
-    //         "longitude": 19.833549
-    //     },
-    //     destination: {
-    //         address: "Bulevar oslobodjenja 46",
-    //         "latitude": 55.267136,
-    //         "longitude": 19.833549
-    //     },
-    // },
-    //     {
-    //         id: "ride2",
-    //         price: 1250,
-    //         startDate: new Date("07/21/2024"),
-    //         departure: {
-    //             address: "Bulevar oslobodjenja 46",
-    //             "latitude": 45.267136,
-    //             "longitude": 16.833549
-    //         },
-    //         destination: {
-    //             address: "Bulevar oslobodjenja 46",
-    //             "latitude": 55.267136,
-    //             "longitude": 18.833549
-    //         },
-    //
-    //     },
-    //     {
-    //         id: "ride3",
-    //         price: 1250,
-    //         startDate: new Date("07/21/2024"),
-    //         departure: {
-    //             address: "Bulevar oslobodjenja 46",
-    //             "latitude": 45.267136,
-    //             "longitude": 22.833549
-    //         },
-    //         destination: {
-    //             address: "Bulevar oslobodjenja 46",
-    //             "latitude": 45.267136,
-    //             "longitude": 30.833549
-    //         },
-    //
-    //     }]
+
+
+    ngAfterViewInit(): void {
+        let DefaultIcon = L.icon({
+            iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+        });
+
+        L.Marker.prototype.options.icon = DefaultIcon;
+        this.initMap();
+    }
 
     rides: Ride[] | undefined
 
     drawRoute(event: Event) {
         let id = (event.currentTarget as HTMLInputElement).id;
-        console.log(id);
+        // @ts-ignore
+        for (let i = 0; i < this.rides?.length; i++) {
+            // @ts-ignore
+            if (id == this.rides[i].id){
+                if (this.rides) {
+                    this.addRoute(this.rides[i].route.locations[0].latitude, this.rides[i].route.locations[0].longitude, this.rides[i].route.locations[1].latitude, this.rides[i].route.locations[1].longitude);
+                    break;
+                }
+            }
+        }
     }
 
     getCards() {
@@ -77,6 +48,27 @@ export class DriverRideHistoryComponent {
             this.rides = value.results;
         });
 
+    }
+
+    initMap(): void {
+        this.map = L.map("map", {
+            center: [45.2396, 19.8227],
+            zoom: 13,
+        });
+
+        const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            minZoom: 3,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
+
+        tiles.addTo(this.map);
+    }
+
+    private addRoute(startLat: number, startLng: number, endLat: number, endLng: number): void {
+        L.Routing.control({
+            waypoints: [L.latLng(startLat, startLng), L.latLng(endLat, endLng)],
+        }).addTo(this.map);
     }
 }
 
