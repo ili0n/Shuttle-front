2552236b6fb309c40a1c439e7e08b4957ab107fa
magley <60552482+magley@users.noreply.github.com>
2023-01-20 10:27:21 +0100

    feat: Implement ride review in passenger history

--- Shuttle-front/src/app/passenger/passenger-history/passenger-history-ride-rate/passenger-history-ride-rate.component.ts
+++ Shuttle-front/src/app/passenger/passenger-history/passenger-history-ride-rate/passenger-history-ride-rate.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { ReviewPairDTO, ReviewService } from 'src/app/review/review.service';
+import { RideRateDialogComponent, ReviewDialogResult } from 'src/app/ride/ride-rate-dialog/ride-rate-dialog.component';
 import { Ride, RideStatus } from 'src/app/ride/ride.service';
 import { RideOrderAgain } from '../passenger-history.component';
 
@@ -8,15 +12,47 @@ import { RideOrderAgain } from '../passenger-history.component';
   templateUrl: './passenger-history-ride-rate.component.html',
   styleUrls: ['./passenger-history-ride-rate.component.css']
 })
-export class PassengerHistoryRideRateComponent {
+export class PassengerHistoryRideRateComponent implements OnInit, OnChanges {
     @Input() public ride: Ride | null = null;
+    private rideReview: ReviewPairDTO | null = null;
 
     constructor(
-        private router: Router
+        private router: Router,
+        private reviewService: ReviewService,
+        private authService: AuthService,
+        private dialog: MatDialog,
     ) {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['ride']) {
+            this.fetchMyReview();
+        }
+    }
+
+    ngOnInit(): void {}
+
+    private fetchMyReview(): void {
+        const myId = this.authService.getUserId();
+
+        this.reviewService.findByRide(this.ride!.id).subscribe({
+            next: (allReviews) => {
+                const review: ReviewPairDTO | undefined = allReviews.find(
+                    rr => rr.driverReview.passenger.id == myId || rr.vehicleReview.passenger.id == myId
+                );
+                if (review) {
+                    this.rideReview = review;
+                } else {
+                    this.rideReview = null;
+                }
+            },
+            error: (err) => {
+                console.error(err);
+            }
+        });
+    }
     
     protected isRideRated(): boolean {
-        return false;
+        return this.rideReview != null;
     }
 
     protected canRateRide(): boolean {
@@ -24,6 +60,9 @@ export class PassengerHistoryRideRateComponent {
             return false;
         }
         if (this.ride.status != RideStatus.Finished) {
+            return false;
+        }
+        if (this.isRideRated()) {
             return false;
         }
         const dateFinished = new Date(this.ride.endTime);
@@ -37,19 +76,48 @@ export class PassengerHistoryRideRateComponent {
     protected tooLateToRateRide(): boolean {
         return !this.canRateRide() && !this.isRideRated();
     }
+
     protected viewRating(): void {
-        console.log("viewRating");
+        const dialogRef = this.dialog.open(RideRateDialogComponent, { 
+            data: {
+                currentReview: this.rideReview,
+                readonly: true,
+            }
+        });
     }
 
     protected leaveRating(): void {
-        console.log("leaveRating");
+        const dialogRef = this.dialog.open(RideRateDialogComponent);
+        dialogRef.afterClosed().subscribe(result => {
+            if (result == undefined) {
+                return;
+            }
+            const reviews: ReviewDialogResult = result;
+
+            const reviewVehicle = reviews.vehicle;
+            if (reviewVehicle.rating != 0) {
+                this.reviewService.leaveReviewVehicle(this.ride!.id, reviewVehicle).subscribe({
+                    next: (result) => {
+                        const reviewDriver = reviews.driver;
+                        if (reviewDriver.rating != 0) {
+                            this.reviewService.leaveReviewDriver(this.ride!.id, reviewDriver).subscribe({
+                                next: (result2) => {
+                                    // There's a review now, so fetch it which'll hide the 'Leave a
+                                    // review' button.
+                                    this.fetchMyReview();
+                                },
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 
     protected orderAgain(): void {
         if (this.ride == null) {
             return;
         }
-        console.log(this.ride);
 
         const params: RideOrderAgain = {
             baby: this.ride.babyTransport,
@@ -59,10 +127,7 @@ export class PassengerHistoryRideRateComponent {
             dest: this.ride.locations.at(-1)!.destination.address
         }
 
-        this.router.navigate(
-            ['passenger/home'],
-            { queryParams: params }
-        );
+        this.router.navigate(['passenger/home'], { queryParams: params });
     }
 
     protected rideIsFavorite(): boolean {
--- Shuttle-front/src/app/ride/ride-rate-dialog/ride-rate-dialog.component.ts
+++ Shuttle-front/src/app/ride/ride-rate-dialog/ride-rate-dialog.component.ts
@@ -1,11 +1,16 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ReviewSendDTO } from 'src/app/review/review.service';
+import { ReviewPairDTO, ReviewSendDTO } from 'src/app/review/review.service';
 
 export interface ReviewDialogResult {
     vehicle: ReviewSendDTO,
     driver: ReviewSendDTO,
+}
+
+export interface ReviewDialogParams {
+    currentReview: ReviewPairDTO,
+    readonly: boolean
 }
 
 
@@ -14,12 +19,14 @@ export interface ReviewDialogResult {
   templateUrl: './ride-rate-dialog.component.html',
   styleUrls: ['./ride-rate-dialog.component.css']
 })
-export class RideRateDialogComponent {
+export class RideRateDialogComponent implements OnInit {
     formGroup: FormGroup;
     protected possibleRatings = ['', '1', '2', '3', '4', '5'];
+    private readonly: boolean = false;
+    private currentRating: ReviewPairDTO | null = null;
 
 
-    constructor(public dialogRef: MatDialogRef<RideRateDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: string, private readonly formBuilder: FormBuilder) {
+    constructor(public dialogRef: MatDialogRef<RideRateDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ReviewDialogParams, private readonly formBuilder: FormBuilder) {
         this.formGroup = this.formBuilder.group({
             commentVehicle: [''],
             commentDriver: [''],
@@ -27,8 +34,24 @@ export class RideRateDialogComponent {
             ratingDriver: [],
         });
 
+        this.readonly = data.readonly;
+        this.currentRating = data.currentReview;
+    }
+
+    ngOnInit(): void {
+        if (this.currentRating != null && this.currentRating != undefined) {
+            console.log(this.currentRating);
+
+            this.formGroup.patchValue({
+                commentVehicle: this.currentRating.vehicleReview.rating,
+                commentDriver: this.currentRating.vehicleReview.comment,
+                ratingVehicle: this.currentRating.driverReview.rating,
+                ratingDriver: this.currentRating.driverReview.comment,
+            });
+        }
+
         this.onChangeDriverRating();
-        this.onChangeDriverRating();
+        this.onChangeVehicleRating();
     }
 
     onCancel(): void {
@@ -61,6 +84,12 @@ export class RideRateDialogComponent {
     }
 
     protected onChangeDriverRating(): void {
+        if (this.readonly) {
+            this.formGroup.get('ratingDriver')?.disable();
+            this.formGroup.get('commentDriver')?.disable();
+            return;
+        }
+
         if (!this.hasRatedDriver()) {
             this.formGroup.get('commentDriver')?.disable();
         } else {
@@ -69,6 +98,12 @@ export class RideRateDialogComponent {
     }
 
     protected onChangeVehicleRating(): void {
+        if (this.readonly) {
+            this.formGroup.get('ratingVehicle')?.disable();
+            this.formGroup.get('commentVehicle')?.disable();
+            return;
+        }
+
         if (!this.hasRatedVehicle()) {
             this.formGroup.get('commentVehicle')?.disable();
         } else {
