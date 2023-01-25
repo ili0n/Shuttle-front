6a6b1521348082cffb99d95e8960892a062dbf9b
magley <60552482+magley@users.noreply.github.com>
2023-01-25 21:03:37 +0100

    feat: Fix driver history test imports

--- Shuttle-front/src/app/driver/driver-history/driver-history.component.spec.ts
+++ Shuttle-front/src/app/driver/driver-history/driver-history.component.spec.ts
@@ -4,6 +4,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'src/infrastructure/material.module';
+import { DriverHistoryRideDetailsComponent } from './driver-history-ride-details/driver-history-ride-details.component';
+import { DriverHistoryRidePassengersComponent } from './driver-history-ride-passengers/driver-history-ride-passengers.component';
+import { DriverHistoryRideTableComponent } from './driver-history-ride-table/driver-history-ride-table.component';
 
 import { DriverHistoryComponent } from './driver-history.component';
 
@@ -13,7 +16,13 @@ describe('DriverHistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DriverHistoryComponent ],
+      declarations: [ 
+            DriverHistoryComponent,
+            // You'd mock these.
+            DriverHistoryRideDetailsComponent,
+            DriverHistoryRidePassengersComponent,
+            DriverHistoryRideTableComponent
+        ],
       imports: [
         FormsModule,
         ReactiveFormsModule,
