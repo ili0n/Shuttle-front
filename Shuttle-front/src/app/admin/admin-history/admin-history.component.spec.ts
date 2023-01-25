daa94073987161f11e1e5191b6e9205bac4909af
magley <60552482+magley@users.noreply.github.com>
2023-01-25 21:02:48 +0100

    feat: Fix admin history test imports

--- Shuttle-front/src/app/admin/admin-history/admin-history.component.spec.ts
+++ Shuttle-front/src/app/admin/admin-history/admin-history.component.spec.ts
@@ -1,4 +1,13 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from 'src/infrastructure/material.module';
+import { AdminHistoryRideDetailsComponent } from './admin-history-ride-details/admin-history-ride-details.component';
+import { AdminHistoryRidePassengersComponent } from './admin-history-ride-passengers/admin-history-ride-passengers.component';
+import { AdminHistoryRideTableComponent } from './admin-history-ride-table/admin-history-ride-table.component';
+import { AdminHistoryUserTableComponent } from './admin-history-user-table/admin-history-user-table.component';
 
 import { AdminHistoryComponent } from './admin-history.component';
 
@@ -8,7 +17,23 @@ describe('AdminHistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminHistoryComponent ]
+      declarations: [ 
+        AdminHistoryComponent, 
+        // You'd mock these.
+        AdminHistoryUserTableComponent,
+        AdminHistoryRideDetailsComponent,
+        AdminHistoryRidePassengersComponent,
+        AdminHistoryUserTableComponent,
+        AdminHistoryRideTableComponent,
+    ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+    ]
     })
     .compileComponents();
 
