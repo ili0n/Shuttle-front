3254ac1549cf0aff943719a39426518aa7b036ce
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-01-29 16:48:18 +0100

    Added date range to driver

--- Shuttle-front/src/app/driver/driver-graph/driver-graph/driver-graph.component.ts
+++ Shuttle-front/src/app/driver/driver-graph/driver-graph/driver-graph.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatTable } from '@angular/material/table';
 import { ChartDataSets } from 'chart.js';
 import { AuthService } from 'src/app/auth/auth.service';
@@ -19,57 +20,34 @@ export class DriverGraphComponent {
   columns = ["Label", "Sum", "Average"];
   @ViewChild(MatTable) table?: MatTable<any>;
 
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null, [Validators.required]),
+    end: new FormControl<Date | null>(null, [Validators.required]),
+  });
+
   constructor(
     private rideService: RideService,
     private authService: AuthService,
     private changeDetectorRefs: ChangeDetectorRef
     ){}
 
-  ngOnInit(): void {
-    this.rideService.getDriverGraphData(this.authService.getUserId(), "2021-01-11T17:45:00Z", "2055-01-11T17:45:00Z").subscribe({
+  ngOnInit(): void {let wayBack: Date = new Date();
+    wayBack.setFullYear(2000);
+    let wayBackStr: string = new Date(wayBack.getTime() - (wayBack.getTimezoneOffset() * 60000)).toISOString();
+
+    let wayForward: Date = new Date();
+    wayBack.setFullYear(2055);
+    let wayForwardStr: string = new Date(wayForward.getTime() - (wayForward.getTimezoneOffset() * 60000)).toISOString();
+
+    this.rideService.getDriverGraphData(this.authService.getUserId(), wayBackStr, wayForwardStr).subscribe({
       next: result => {
-        if(result.length === 0){
-          return;
-        }
-
-        let numberOfRides = result.map(entry => entry.numberOfRides);
-        let numberOfRidesData = [{
-          "data": numberOfRides,
-          "label": 'Number of rides',
-          "type": "line",
-          "lineTension": 0 
-         }];
-
-         let costSum = result.map(entry => entry.costSum);
-         let costSumData = [{
-          "data": costSum,
-          "label": 'Money earned',
-          "type": "line",
-          "lineTension": 0 
-         }];
-
-         let length = result.map(entry => entry.length);
-         let lengthData = [{
-          "data": length,
-          "label": 'Length in km',
-          "type": "line",
-          "lineTension": 0 
-         }];
-         //console.log([numberOfRides, costSum, length]);
-
-         let chartLabels = result.map(entry =>  entry.time);
-         //console.log(chartLabels);
-
-         this.numberOfRidesData = numberOfRidesData;
-         this.costSumData = costSumData;
-         this.lengthData = lengthData;
-         this.chartLabels = chartLabels;
-         this.calculateAndAdd(numberOfRides, costSum, length);
+        this.showData(result);
       },
       error: err => console.log(err)
     })
   }
   calculateAndAdd(numberOfRides: number[], costSum: number[], length: number[]) {
+    this.rows = [];
     this.addRow("Number of rides", numberOfRides);
     this.addRow("Money earned", costSum);
     this.addRow("Length", length);
@@ -79,8 +57,67 @@ export class DriverGraphComponent {
   addRow(labelText: string, arr: number[]) {
     let add = (a: number, b: number) => a + b;
     let sum = arr.reduce(add, 0);
-    let avg = sum / arr.length;
+    let avg = arr ? sum / arr.length : 0;
     this.rows.push({labelText: labelText, sum: sum, avg: avg});
+  }
+
+  dateRangeChange(startInput: HTMLInputElement, endInput: HTMLInputElement){
+    console.log(startInput.value);
+    console.log(endInput.value);
+    if(this.range.valid){
+      let start: Date = new Date(startInput.value);
+      let startStr: string = new Date(start.getTime() - (start.getTimezoneOffset() * 60000)).toISOString();
+
+      let end: Date = new Date(endInput.value);
+      let endStr: string = new Date(end.getTime() - (end.getTimezoneOffset() * 60000)).toISOString();
+
+      this.rideService.getDriverGraphData(this.authService.getUserId(), startStr, endStr).subscribe({
+        next: result => {
+          this.showData(result);
+        },
+        error: err => console.log(err)
+      })
+    }
+  }
+
+  showData(result: import("src/app/ride/ride.service").GraphEntry[]) {
+    if(result.length === 0){
+      return;
+    }
+
+    let numberOfRides = result.map(entry => entry.numberOfRides);
+    let numberOfRidesData = [{
+      "data": numberOfRides,
+      "label": 'Number of rides',
+      "type": "line",
+      "lineTension": 0 
+     }];
+
+     let costSum = result.map(entry => entry.costSum);
+     let costSumData = [{
+      "data": costSum,
+      "label": 'Money earned',
+      "type": "line",
+      "lineTension": 0 
+     }];
+
+     let length = result.map(entry => entry.length);
+     let lengthData = [{
+      "data": length,
+      "label": 'Length in km',
+      "type": "line",
+      "lineTension": 0 
+     }];
+     //console.log([numberOfRides, costSum, length]);
+
+     let chartLabels = result.map(entry =>  entry.time);
+     //console.log(chartLabels);
+
+     this.numberOfRidesData = numberOfRidesData;
+     this.costSumData = costSumData;
+     this.lengthData = lengthData;
+     this.chartLabels = chartLabels;
+     this.calculateAndAdd(numberOfRides, costSum, length);
   }
 
 }
