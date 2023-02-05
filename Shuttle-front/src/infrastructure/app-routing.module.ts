e36824869255412f90a5a04719c99481328e907b
magley <60552482+magley@users.noreply.github.com>
2023-02-05 11:28:32 +0100

    feat: Add selecting route from map

--- Shuttle-front/src/infrastructure/app-routing.module.ts
+++ Shuttle-front/src/infrastructure/app-routing.module.ts
@@ -25,6 +25,7 @@ const routes: Routes = [
         loadChildren: () =>
             import('../app/auth/auth.module').then((m) => m.AuthModule),
     },
+    {path: '', redirectTo: 'unregistered', pathMatch: 'full'},
   {path: "register", component: RegisterComponent, canActivate: [LoginGuard]},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: 'reset-password', redirectTo: 'login'},
